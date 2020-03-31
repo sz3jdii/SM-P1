c955e523b4f7029d1049170d2dae8d7d881ab7d6
a_swiatkowski <adam.swiatkowski@projectic.pl>
2020-04-01 01:44:17 +0200

    init

---
+++ app/src/main/java/pl/adamswiatkowski/smp1/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.adamswiatkowski.smp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
